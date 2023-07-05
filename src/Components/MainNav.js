39670766b57576e469bb9c47f163f4e83d9fa08a
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-07-05 13:27:30 +0530

    header and footer added

--- src/Components/MainNav.js
+++ src/Components/MainNav.js
@@ -1,27 +1,28 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import {  makeStyles } from '@material-ui/core/styles';
+
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-const useStyles = makeStyles(
-    {
-        root:{
-            width: 100,
-            position: "fixed"
-        },
-    }
-)
+
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box >
       <BottomNavigation
+      sx={{
+      width:"100%",
+      backgroundColor: "#39445a",
+      boxShadow: "  rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;",
+      position: "fixed",
+      bottom: 0
+
+    }}
         showLabels
         value={value}
         onChange={(event, newValue) => {
